f24ff283c6d25bf3737b3c56c6b95e6d244fb60f
sameermkhan@hotmail.com <Shavane1!>
2021-04-16 02:43:31 -0300

    before i do recycler

--- app/src/main/java/com/ceng319/n01231625/javapersonifyapp/DisplayHardware.java
+++ app/src/main/java/com/ceng319/n01231625/javapersonifyapp/DisplayHardware.java
@@ -5,13 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.hardware.Sensor;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -48,26 +51,36 @@ import java.util.List;
 import java.util.Map;
 
 public class DisplayHardware extends AppCompatActivity {
-    private TextView mTextViewResult;
+   private TextView lightSensorB;
     public RequestQueue queue;
     private FirebaseFunctions mFunctions;
-    private ListView listView;
+    private ListView lv;
     RecyclerView recyclerView;
-    List<Sensors> theSensors;
     Adapter adapter;
+    ArrayList<HashMap<String, String>>  dataList;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mTextViewResult = findViewById(R.id.mTextViewResult);
+
         setContentView(R.layout.activity_display_hardware);
         mFunctions = FirebaseFunctions.getInstance();
-        recyclerView = findViewById(R.id.dataList);
-        // Button buttonParse = findViewById(R.id.getDataRequest);
-        theSensors = new ArrayList<>();
+
+
         jsonParse();
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        adapter = new Adapter(this, theSensors);
+       recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        dataList = new ArrayList<>();
+
+     //   lv = findViewById(R.id.list);
+        ListAdapter adapter = new SimpleAdapter(
+                DisplayHardware.this, dataList,
+                R.layout.list_item, new String[]{"lightSensor", "coreVoltage",
+                "temperature","gesture"}, new int[]{R.id.coreVoltageB,
+                R.id.lightSensorB, R.id.TemperatureB,R.id.GESTURE});
+
+        lv.setAdapter(adapter);
+
+
 
 
     }
@@ -92,32 +105,56 @@ public class DisplayHardware extends AppCompatActivity {
                 e.printStackTrace();
             }
 
-
             @Override
             public void onResponse(Call call, Response response) throws IOException {
                 if (response.isSuccessful()) {
                     final String myResponse = response.body().string();
+                   // Log.e("BEFORE PARSE",""+myResponse);
 
-                    DisplayHardware.this.runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
+
                             try {
+                                JSONObject jsonObj = new JSONObject(myResponse);
 
-                                JSONObject json = new JSONObject(myResponse);
-                                mTextViewResult.setText(json.getJSONObject("data").getString("LIGHT_SENSOR") + " " + json.getJSONObject("SENSORS").getInt("TIMESTAMP"));
+                                // Getting JSON Array node
+                                JSONArray data = jsonObj.getJSONArray("data");
+
+
+
+
+                                for (int i = 0; i < data.length(); i++) {
+                                    JSONObject d = data.getJSONObject(i);
+
+
+
+                                    String lightSensorN = d.getString("TIMESTAMP");
+                                    Log.e("parsing light",""+lightSensorN);
+
+
+
+                               lightSensorB.setText(lightSensorN);
+                               //     String coreVoltageN = d.getString("CORE_VOLTAGE");
+                               //     String temperatureN= d.getString("TEMPERATURE");
+                               //     String gestureN = d.getString("GESTURE");
+                                    //
+                                    //          JSONObject nodeTime = d.getJSONObject("LIGHT_SENSOR");
+                                    //        String timestamp = phone.getString("TIMESTAMP");
+                                    // tmp hash map for single contact
+
+
+                                  Log.e("Parsing node",""+lightSensorN);
+
+                                }
+
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
-                        }
-                    });
+
                 }
             }
-
-
-
         });
     }
 }
+
 
 
 
